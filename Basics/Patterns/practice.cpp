f311c863b5d655c58b58d20fbe06699b0e7540b4
Chetan-Vashisht <chetanvashishtc@gmail.com>
2025-03-13 21:41:59 +0530

    Practice

---
+++ Basics/Patterns/practice.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "_";
+        }
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+            /* code */
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        // cout<<endl;
+        for (int j = n; j > n - i; j--)
+        {
+            cout << "_";
+        }
+        // for (int j = 0; j < i+1; j++)
+        // {
+        //     cout<<"*";
+        // }
+        // for (int  j = 0; j < i; j++)
+        // {
+        //     cout<<"*";
+        //     /* code */
+        // }    
+        cout<<endl;
+    }
+
+    return 0;
+}
--- Basics/Patterns/tempCodeRunnerFile.cpp
+++ Basics/Patterns/tempCodeRunnerFile.cpp
@@ -1,9 +1,4 @@
- // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 1; j <= i+1; j++)
-    //     {
-    //         cout<<j;
-    //     }
-    //     cout<<endl;
-    // }
-    
+ for (int j = 0; j < i+1; j++)
+        {
+            cout<<"*";
+        }
