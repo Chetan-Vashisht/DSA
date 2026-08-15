e589cd8e33a68973c3547763fcf38c549d59f477
Chetan Vashisht <chetanvashishtc@gmail.com>
2026-08-15 21:41:29 +0530

    Some changes in Array Sort

--- 03_Recursion/05_checkArrayIsSorted.cpp
+++ 03_Recursion/05_checkArrayIsSorted.cpp
@@ -1,27 +1,27 @@
-#include <bits/stdc++.h>
-#include <iostream>
-using namespace std;
-bool isSorted(int n , vector<int> v){
-    if(n==0||n==1)return true;
-    if(v[n-1]>=v[n-1]) return true;
+// #include <bits/stdc++.h>
+// #include <iostream>
+// using namespace std;
+// // bool isSorted(int n , vector<int> v){
+// //     if(n==0||n==1)return true;
+// //     if(v[n-1]>=v[n-1]) return true;
 
-}
-int isSorted {
+// // }
+// // int isSorted {
 
-    ;
-}
+// //     ;
+// // }
 
-int main() {
+// int main() {
    
-    vector<int> vec={1,2,3,4,5};
-    int sz = vec.size();
+//     vector<int> vec={1,2,3,4,5};
+//     int sz = vec.size();
 
-    cout<<isSorted(sz,vec);
-
-
+//     cout<<isSorted(sz,vec);
 
 
 
 
-    return 0;
-}
+
+
+//     return 0;
+// }
