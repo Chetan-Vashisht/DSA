872bf5f29a49b40f5ba4879d833bf12a4090fb59
Chetan Vashisht <chetanvashishtc@gmail.com>
2026-08-17 14:48:29 +0530

    Added Leetcode Code to notes

--- 03_Recursion/06_binaryRecursice.cpp
+++ 03_Recursion/06_binaryRecursice.cpp
@@ -1,21 +1,41 @@
-#include <bits/stdc++.h>
-#include <iostream>
-using namespace std;
-int binarySearch(vector<int>v,int s,int st,int end,int target){
-    int mid =st+end/2;
-    if (mid == target) return;
-    if(target<mid) binarySearch(v,s,st,mid-1,target);
-    else binarySearch(v,s,mid+1,end,target);
-}
+// #include <bits/stdc++.h>
+// #include <iostream>
+// using namespace std;
+// int binarySearch(vector<int>v,int s,int st,int end,int target){
+//     int mid =st+end/2;
+//     if (mid == target) return;
+//     if(target<mid) binarySearch(v,s,st,mid-1,target);
+//     else binarySearch(v,s,mid+1,end,target);
+// }
 
-int main() {
-    vector<int>v ={1,2,3,4,5,6,7,9};
-    int s=v.size();
-    int target=3;
-    auto st=v.begin();
-    auto e=v.end();
-    cout<<binarySearch(v,s,v.begin(),)
+// int main() {
+//     vector<int>v ={1,2,3,4,5,6,7,9};
+//     int s=v.size();
+//     int target=3;
+//     auto st=v.begin();
+//     auto e=v.end();
+//     cout<<binarySearch(v,s,v.begin(),)
 
+
+// Leetcode
+class Solution {
+public:
+    int binarySearch(vector<int>& arr, int size, int start, int end, int target) {
+        if (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target)
+                return mid;
+            if (target < arr[mid])
+                return binarySearch(
+                    arr, size, start, mid - 1,target) ;
+            else return binarySearch(arr, size, mid +1, end,target);
+        }
+        return -1;
+    }
+    int search(vector<int>& arr, int target) {
+        return binarySearch(arr, arr.size(), 0, arr.size()-1, target);
+    }
+};
     
 
 
@@ -23,5 +43,5 @@ int main() {
 
 
 
-    return 0;
-}
+//     return 0;
+// }
