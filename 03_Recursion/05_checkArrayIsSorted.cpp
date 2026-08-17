9c36191e87107f86c8bfd887a5a50fd4467f55b8
Chetan Vashisht <chetanvashishtc@gmail.com>
2026-08-17 14:46:16 +0530

    Recursive Bianry

--- 03_Recursion/05_checkArrayIsSorted.cpp
+++ 03_Recursion/05_checkArrayIsSorted.cpp
@@ -1,27 +1,22 @@
-// #include <bits/stdc++.h>
-// #include <iostream>
-// using namespace std;
-// // bool isSorted(int n , vector<int> v){
-// //     if(n==0||n==1)return true;
-// //     if(v[n-1]>=v[n-1]) return true;
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+bool isSorted(vector<int>v,int n){
+    if(n==1||n==0)return true;
 
-// // }
-// // int isSorted {
-
-// //     ;
-// // }
-
-// int main() {
-   
-//     vector<int> vec={1,2,3,4,5};
-//     int sz = vec.size();
-
-//     cout<<isSorted(sz,vec);
+    return (v[n-1]>=v[n-2]) && isSorted(v,n-1);
+}
+int main() {
+    vector<int> vec={1,2,3,5,6,3};
+    int sz = vec.size();
+    int st=isSorted(vec,sz);
+    if(st==0)cout<<"False"<<endl;
+    else cout<<"true"<<endl;
 
 
 
 
 
 
-//     return 0;
-// }
+    return 0;
+}
---
+++ 03_Recursion/06_binaryRecursice.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int binarySearch(vector<int>v,int s,int st,int end,int target){
+    int mid =st+end/2;
+    if (mid == target) return;
+    if(target<mid) binarySearch(v,s,st,mid-1,target);
+    else binarySearch(v,s,mid+1,end,target);
+}
+
+int main() {
+    vector<int>v ={1,2,3,4,5,6,7,9};
+    int s=v.size();
+    int target=3;
+    auto st=v.begin();
+    auto e=v.end();
+    cout<<binarySearch(v,s,v.begin(),)
+
+    
+
+
+
+
+
+
+    return 0;
+}
