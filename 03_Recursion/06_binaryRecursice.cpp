663453f54f4e2d91117521156b69edfb93b0e249
Chetan Vashisht <chetanvashishtc@gmail.com>
2026-08-18 15:36:41 +0530

    Learingi Prining Subsets

--- 03_Recursion/06_binaryRecursice.cpp
+++ 03_Recursion/06_binaryRecursice.cpp
@@ -1,41 +1,64 @@
+// // #include <bits/stdc++.h>
+// // #include <iostream>
+// // using namespace std;
+// // int binarySearch(vector<int>v,int s,int st,int end,int target){
+// //     int mid =st+end/2;
+// //     if (mid == target) return;
+// //     if(target<mid) binarySearch(v,s,st,mid-1,target);
+// //     else binarySearch(v,s,mid+1,end,target);
+// // }
+
+// // int main() {
+// //     vector<int>v ={1,2,3,4,5,6,7,9};
+// //     int s=v.size();
+// //     int target=3;
+// //     auto st=v.begin();
+// //     auto e=v.end();
+// //     cout<<binarySearch(v,s,v.begin(),)
+
+// // Leetcode
+// // class Solution {
+// // public:
+// //     int binarySearch(vector<int>& arr, int size, int start, int end, int target) {
+// //         if (start <= end) {
+// //             int mid = (start + end) / 2;
+// //             if (arr[mid] == target)
+// //                 return mid;
+// //             if (target < arr[mid])
+// //                 return binarySearch(
+// //                     arr, size, start, mid - 1,target) ;
+// //             else return binarySearch(arr, size, mid +1, end,target);
+// //         }
+// //         return -1;
+// //     }
+// //     int search(vector<int>& arr, int target) {
+// //         return binarySearch(arr, arr.size(), 0, arr.size()-1, target);
+// //     }
+// // };
+
+// //     return 0;
+// // }
 // #include <bits/stdc++.h>
 // #include <iostream>
 // using namespace std;
-// int binarySearch(vector<int>v,int s,int st,int end,int target){
-//     int mid =st+end/2;
-//     if (mid == target) return;
-//     if(target<mid) binarySearch(v,s,st,mid-1,target);
-//     else binarySearch(v,s,mid+1,end,target);
+// int binarySearch(vector<int> v, int start, int end, int target){
+//     if(start<=end){
+//     int mid = (start+end)/2;
+//     if (v[mid]==target) return mid;
+//     if (target<mid) return binarySearch(v,start,mid-1,target);
+//     else return binarySearch(v,mid+1,end,target);
+//     }
+//     else return -1;
 // }
 
 // int main() {
-//     vector<int>v ={1,2,3,4,5,6,7,9};
-//     int s=v.size();
-//     int target=3;
-//     auto st=v.begin();
-//     auto e=v.end();
-//     cout<<binarySearch(v,s,v.begin(),)
+//     vector<int> v={1,2,3,4,5,6,8};
+//     auto start=v.begin();
+//     auto end=v.end()-1;
+//     int target=5;
+//     binarySearch(v,start,end,target);
+    
 
-
-// Leetcode
-class Solution {
-public:
-    int binarySearch(vector<int>& arr, int size, int start, int end, int target) {
-        if (start <= end) {
-            int mid = (start + end) / 2;
-            if (arr[mid] == target)
-                return mid;
-            if (target < arr[mid])
-                return binarySearch(
-                    arr, size, start, mid - 1,target) ;
-            else return binarySearch(arr, size, mid +1, end,target);
-        }
-        return -1;
-    }
-    int search(vector<int>& arr, int target) {
-        return binarySearch(arr, arr.size(), 0, arr.size()-1, target);
-    }
-};
     
 
 
@@ -45,3 +68,4 @@ public:
 
 //     return 0;
 // }
+
---
+++ 03_Recursion/07_printSubset.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+void subset(vector<int>v,vector<int> ans,int i){
+    if(i==v.size()) return;
+    // incriment
+    ans.push_back(v[i]);
+    subset(v,ans,i+1);
+    ans.pop_back();
+
+}
+
+int main() {
+
+    
+
+
+
+
+
+
+    return 0;
+}
