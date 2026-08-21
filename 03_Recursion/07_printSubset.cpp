46275444d1b26edf5d0ba1000b9124209d5ce53e
Chetan Vashisht <chetanvashishtc@gmail.com>
2026-08-21 22:32:36 +0530

    Changes

--- 03_Recursion/07_printSubset.cpp
+++ 03_Recursion/07_printSubset.cpp
@@ -1,16 +1,16 @@
-// #include <bits/stdc++.h>
-// #include <iostream>
-// using namespace std;
-// void subset(vector<int>v,vector<int> ans,int i){
-//     if(i==v.size()) return;
-//     // incriment
-//     ans.push_back(v[i]);
-//     subset(v,ans,i+1);
-//     ans.pop_back();
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+void subset(vector<int>v,vector<int> ans,int i){
+    if(i==v.size()) return;
+    // incriment
+    ans.push_back(v[i]);
+    subset(v,ans,i+1);
+    ans.pop_back();
 
-// }
+}
 
-// int main() {
+int main() {
 
     
 
@@ -19,5 +19,5 @@
 
 
 
-//     return 0;
-// }
+    return 0;
+}
