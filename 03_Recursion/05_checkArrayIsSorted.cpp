a12a845baf35fe235291bbca9496f1fb56c466cf
Chetan Vashisht <chetanvashishtc@gmail.com>
2026-08-31 21:18:38 +0530

    _

--- 03_Recursion/05_checkArrayIsSorted.cpp
+++ 03_Recursion/05_checkArrayIsSorted.cpp
@@ -1,22 +1,22 @@
-#include <bits/stdc++.h>
-#include <iostream>
-using namespace std;
-bool isSorted(vector<int>v,int n){
-    if(n==1||n==0)return true;
+// #include <bits/stdc++.h>
+// #include <iostream>
+// using namespace std;
+// bool isSorted(vector<int>v,int n){
+//     if(n==1||n==0)return true;
 
-    return (v[n-1]>=v[n-2]) && isSorted(v,n-1);
-}
-int main() {
-    vector<int> vec={1,2,3,5,6,3};
-    int sz = vec.size();
-    int st=isSorted(vec,sz);
-    if(st==0)cout<<"False"<<endl;
-    else cout<<"true"<<endl;
-
-
+//     return (v[n-1]>=v[n-2]) && isSorted(v,n-1);
+// }
+// int main() {
+//     vector<int> vec={1,2,3,5,6,3};
+//     int sz = vec.size();
+//     int st=isSorted(vec,sz);
+//     if(st==0)cout<<"False"<<endl;
+//     else cout<<"true"<<endl;
 
 
 
 
-    return 0;
-}
+
+
+//     return 0;
+// }
