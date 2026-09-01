0740930a45b01a6cb539198c92be166768b4c104
Chetan Vashisht <chetanvashishtc@gmail.com>
2026-09-01 20:41:59 +0530

    Arrays

--- 04_Arrays/findLargestElimentInArray.cpp
+++ 04_Arrays/findLargestElimentInArray.cpp
@@ -1,18 +1,18 @@
-// #include <bits/stdc++.h>
-// #include <iostream>
-// using namespace std;
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
 
-// int main() {
-//     vector<int> v={1,2,5,3,6,4};
-//     int le=v[0],sz=v.size()-1;
-//     // Brute Foce
-//         // Just sort the array/vecotr and than print the last element i.e. v.end()-1
-//         //  T.C. is More than best approach
-//     // Best Approach
-//     for(int i=0;i<sz;i++){
-//         if(v[i]>le)le=v[i];
-//     }
-//     cout<<le<<endl;
+int main() {
+    vector<int> v={1,2,5,3,6,4};
+    int le=v[0],sz=v.size()-1;
+    // Brute Foce
+        // Just sort the array/vecotr and than print the last element i.e. v.end()-1
+        //  T.C. is More than best approach
+    // Best Approach
+    for(int i=0;i<sz;i++){
+        if(v[i]>le)le=v[i];
+    }
+    cout<<le<<endl;
     
 
 
@@ -20,5 +20,5 @@
 
 
 
-//     return 0;
-// }
+    return 0;
+}
