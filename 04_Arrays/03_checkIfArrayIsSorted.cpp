f1c5998359ca807eb58b45ed7d6398b218f9b778
Chetan Vashisht <chetanvashishtc@gmail.com>
2026-09-08 11:48:27 +0530

    Sorted and dupicates

--- 04_Arrays/03_checkIfArrayIsSorted.cpp
+++ 04_Arrays/03_checkIfArrayIsSorted.cpp
@@ -1,16 +1,24 @@
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-bool isSorted(vector<int>v){
+// bool isSorted(vector<int>v){
+//     for(int i=0;i<v.size()-1;i++){
+//         if(v[i]>v[i+1])return false;
+//     }
+//     return true;
+// }
+bool isSorted(vector<int> v){
     for(int i=0;i<v.size()-1;i++){
         if(v[i]>v[i+1])return false;
     }
     return true;
 }
 
+
+
 int main() {
 
-    vector <int>v={2,4,6,5,9,20,1};
+    vector <int>v={2,4,5,9,20};
     cout<<isSorted(v)<<endl;
     
 
---
+++ 04_Arrays/04_removeDuplicatesInPlace.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    vector<int> v={1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4,4};
+    // Brute force
+    // set <int> s;
+    // for(int i =0; i<v.size();i++){
+    //     s.emplace(v[i]);
+
+    // }
+    // cout<<s.size();
+
+
+
+    
+        return 0;
+
+}
