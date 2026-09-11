2d9e5475cf959edb0d39394b620a8703976b0a98
Chetan Vashisht <chetanvashishtc@gmail.com>
2026-09-11 20:47:03 +0530

    sorted

--- 04_Arrays/03_checkIfArrayIsSorted.cpp
+++ 04_Arrays/03_checkIfArrayIsSorted.cpp
@@ -1,36 +1,36 @@
-// // #include <bits/stdc++.h>
-// // #include <iostream>
-// // using namespace std;
-// // // bool isSorted(vector<int>v){
-// // //     for(int i=0;i<v.size()-1;i++){
-// // //         if(v[i]>v[i+1])return false;
-// // //     }
-// // //     return true;
-// // // }
-// // bool isSorted(vector<int> v){
+// #include <bits/stdc++.h>
+// #include <iostream>
+// using namespace std;
+// // bool isSorted(vector<int>v){
 // //     for(int i=0;i<v.size()-1;i++){
 // //         if(v[i]>v[i+1])return false;
 // //     }
 // //     return true;
 // // }
+// bool isSorted(vector<int> v){
+//     for(int i=0;i<v.size()-1;i++){
+//         if(v[i]>v[i+1])return false;
+//     }
+//     return true;
+// }
 
 
-
-// // int main() {
-
-// //     vector <int>v={2,4,5,9,20};
-// //     cout<<isSorted(v)<<endl;
-// //     return 0;
-// // }
-
-// #include <bits/stdc++.h>
-// #include <iostream>
-// using namespace std;
 
 // int main() {
 
-//     // Obtamised Approach 
-//     vector <int> v={1,2,3,6,7,5};
+//     vector <int>v={2,4,5,9,20};
+//     cout<<isSorted(v)<<endl;
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Obtamised Approach 
+    vector <int> v={1,2,3,6,7,5};
     
 
 
@@ -38,5 +38,5 @@
 
 
 
-//     return 0;
-// }
+    return 0;
+}
