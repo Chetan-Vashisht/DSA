8f28132aecc83019499b70d7f3e3efad73c486fc
Chetan Vashisht <chetanvashishtc@gmail.com>
2026-09-21 20:55:42 +0530

    some Changes

--- 04_Arrays/04_removeDuplicatesInPlace.cpp
+++ 04_Arrays/04_removeDuplicatesInPlace.cpp
@@ -1,23 +1,23 @@
-#include <bits/stdc++.h>
-#include <iostream>
-using namespace std;
+// #include <bits/stdc++.h>
+// #include <iostream>
+// using namespace std;
 
-int main() {
+// int main() {
 
-    vector<int> v={1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4,4};
-    // Brute force
-    // set <int> s;
-    // for(int i =0; i<v.size();i++){
-    //     s.emplace(v[i]);
+//     vector<int> v={1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4,4};
+//     // Brute force
+//     // set <int> s;
+//     // for(int i =0; i<v.size();i++){
+//     //     s.emplace(v[i]);
 
-    // }
-
-    
-    // cout<<s.size();
-
-
+//     // }
 
     
-        return 0;
+//     // cout<<s.size();
 
-}
+
+
+    
+//         return 0;
+
+// }
